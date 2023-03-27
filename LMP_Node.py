9f7c83b53fb6050c02984f2e8442ecaf34a5ba25
Julian Peters <uni@julian-peters.com>
2023-03-27 17:42:22 +0200

    done for today

--- LMP_Node.py
+++ LMP_Node.py
@@ -11,33 +11,32 @@ class lammpsnode(Node):
         lmp_exe: This is the name or path of the LAMMPS executable. Either path
             to executable, "lmp" or "lamp_<machine>". 
             See https://docs.lammps.org/Run_basics.html for more information
-        lmp_params: To be able to change parameters with DVC and not have to change
-            them manually in the input script, a params file in yaml format and
-            corresponding template file must be provided.
-        lmp_template: In combination with the params file this will be the input
-            script for the lammps simulation
+        lmp_params: Path to file. To be able to change parameters with DVC and not 
+            have to change them manually in the input script, a params file in yaml 
+            format and corresponding template file must be provided.
+        lmp_template: Path to file. In combination with the params file this will 
+            be the input script for the LAMMPS simulation
 
     Returns:
         #TODO
     """
 
     lmp_exe = meta.Text("lmp_serial")
-    lmp_params = dvc.params("lammps.yaml")
-    lmp_template = dvc.deps("NPT.in")
+    lmp_params = dvc.params()
+    lmp_template = dvc.deps()
 
 
     def _post_init_(self):
         params = yaml.safe_load(self.lmp_params)
-        #loader = FileSystemLoader()
-        #env = Environment(loader=loader)
-        print(params)
+        loader = FileSystemLoader(".")
+        env = Environment(loader=loader, trim_blocks=True, lstrip_blocks=True)
+        template = env.get_template(self.lmp_template) 
+        self.input_script = template.render(params)
 
     def run(self):
-                
-
         """
         proc = subprocess.Popen(
-                [self.lmp_exe, "-in", self.lmp_input_file], 
+                [self.lmp_exe, "-in", self.lmp_input_script], 
                 stdout=subprocess.PIPE, 
                 stderr=subprocess.PIPE,
                 )
@@ -48,6 +47,7 @@ class lammpsnode(Node):
         """
         pass
 
+
 if __name__ == "__main__":
-    lmp = lammpsnode()
+    lmp = lammpsnode(lmp_params="lammps.yaml", lmp_template="templates/npt.lmp")
     lmp.write_graph()
