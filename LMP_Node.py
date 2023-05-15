41c11c7cffd8bb2008eefde4ac277d928c87610d
Julian Peters <uni@julian-peters.com>
2023-05-15 22:05:06 +0200

    added (not yet tested) ase atoms support

--- LMP_Node.py
+++ LMP_Node.py
@@ -1,9 +1,10 @@
 import pathlib
 import subprocess
-
+import ase
+import ase.io
 import yaml
 from jinja2 import Environment, FileSystemLoader
-from zntrack import Node, dvc, meta, utils
+from zntrack import Node, dvc, meta, utils, zn
 
 
 class LammpsSimulator(Node):
@@ -34,13 +35,41 @@ class LammpsSimulator(Node):
         output to the specified directory.
 
     """
-
+	
+    lmp_directory: str = dvc.outs(utils.nwd / "lammps")
     lmp_exe: str = meta.Text("lmp_serial")
-    lmp_params: str = dvc.params()
-    lmp_template: str = dvc.deps()
     skiprun: bool = False
 
-    lmp_directory: str = dvc.outs(utils.nwd / "lammps")
+    #inputs
+    atoms: ase.Atoms = zn.deps(None)
+    atoms_file = dvc.deps(None) #input trajectory 
+
+    #outputs
+    dump_file = dvc.outs(utils.nwd / "NPT.lammpstraj")
+    log_file = dvc.outs(utils.nwd / "NPT.log")
+    
+
+    lmp_params: str = dvc.params()
+    lmp_template: str = dvc.deps()
+    
+    def _post_init_(self):
+   		# Check if atoms were provided:
+        if self.atoms is None and self.atoms_file is None:
+            raise TypeError("Both atoms and atoms_file mustn't be None")
+        if self.atoms is not None and self.atoms_file is not None:
+            raise TypeError(
+                "Atoms and atoms_file are mutually exclusive. Please only provide one"
+            ) 
+
+    def get_atoms(self):
+        #look where to get the input_trajectory (either ase.Atoms or file)
+        if self.atoms is None:
+            self.atoms_file = pathlib.Path(self.atoms_file).resolve().as_posix()
+        if self.atoms_file is None:
+            #if not atoms_file is provided, input_trajectory has to come from ase.Atoms, which have to be written to a file.
+            ase.io.write(self.lmp_directory / "atoms.xyz", self.atoms)
+            self.atoms_file = pathlib.Path(self.lmp_directory / "atoms.xyz").resolve().as_posix()
+
 
     def create_input_script(self):
         # Get parameter from yaml:
@@ -49,12 +78,10 @@ class LammpsSimulator(Node):
 
         # Resolve paths for input files
         input_dict = {}
-        for key in params["files"]["inputs"]:
-            input_dict[key] = (
-                pathlib.Path(params["files"]["inputs"][key]).resolve().as_posix()
-            )
-        for key in params["files"]["outputs"]:
-            input_dict[key] = params["files"]["outputs"][key]
+        input_dict["log_file"] = self.log_file.resolve().as_posix()
+        input_dict["dump_file"] = self.dump_file.resolve().as_posix()
+        input_dict["input_trajectory"] = self.atoms_file
+
         for key in params["sim_parameters"]:
             input_dict[key] = params["sim_parameters"][key]
 
@@ -70,14 +97,16 @@ class LammpsSimulator(Node):
 
     def run(self):
         self.lmp_directory.mkdir(exist_ok=True)  # create output directory
+        self.get_atoms()
         self.create_input_script()
+
         if self.skiprun:
             print("Skipping simulation ...")
             cmd = [self.lmp_exe, "-sr" "-in", "input.script"]
         else:
             print("Simulating ...")
             cmd = [self.lmp_exe, "-in", "input.script"]
-
+        
         subprocess.run(
             cmd,
             cwd=self.lmp_directory,
@@ -85,12 +114,12 @@ class LammpsSimulator(Node):
             stderr=subprocess.PIPE,
             text=True,
         )
-        # TODO Find a Way to get live output from run or Popen
-        # print(proc.stdout)
+
 
 
 if __name__ == "__main__":
     lmp = LammpsSimulator(
+        atoms_file = "NaCl.xyz",
         lmp_params="npt_params.yaml",
         lmp_template="templates/npt.lmp",
     )
